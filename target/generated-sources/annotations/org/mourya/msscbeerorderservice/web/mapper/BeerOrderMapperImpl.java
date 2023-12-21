640f94f23944365d09fd1c64ce59bca231375ee6
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2023-12-21 23:30:21 +0530

    optimized pom

--- target/generated-sources/annotations/org/mourya/msscbeerorderservice/web/mapper/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/org/mourya/msscbeerorderservice/web/mapper/BeerOrderMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-20T14:02:09+0530",
+    date = "2023-12-21T23:28:56+0530",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
